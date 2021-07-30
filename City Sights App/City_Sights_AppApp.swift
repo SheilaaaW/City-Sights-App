31c2f555a03205d0f748eb93f97ac9fe003a89da
Sheila <sheilawang93@gmail.com>
2021-07-30 14:51:17 -0700

    Initial Commit

---
+++ City Sights App/City_Sights_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  City_Sights_AppApp.swift
+//  City Sights App
+//
+//  Created by Sheila Wang on 2021-07-30.
+//
+
+import SwiftUI
+
+@main
+struct City_Sights_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
